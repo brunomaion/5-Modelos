a10b423f541064ddbd28ed56e3c71912f2c8d8a3
brunomaion <brunosmaion@hotmail.com>
2024-04-16 08:38:14 -0300

    xgboost

--- auxiliar.py
+++ auxiliar.py
@@ -1,106 +1,76 @@
-#RF
+#XG
 
 
-mseRF = []
-rmseRF= []
-maeRF= []
+mseXG = []
+rmseXG= []
+maeXG= []
 
-p_RFcriterion = []
-p_RFn_estimators  = []
-p_RFmin_samples_leaf = []
+p_XGbooster = []
 
-param_grid = {
-    'criterion': [(100,), (200,), (300,), (400,), (500,)],
-    'n_estimators': ['relu', 'logistic', 'tanh', 'identity'],
-    'min_samples_leaf': [400,600,800,1000],
-}
-melhor_validacao_RF = 1000
-best_paramsRF = {}
+param_grid = {'booster': ['gblinear', 'gbtree']}
+
+melhor_validacao_XG = 1000
+best_paramsXG = {}
 
 
 
 for i in range(20):
     print(i+1)
-    for criterion in param_grid['criterion']:
-        for n_estimators in param_grid['n_estimators']:
-            for min_samples_leaf in param_grid['min_samples_leaf']:
-                RF = RandomForestRegressor(
-                    criterion=criterion,
-                    n_estimators=n_estimators,
-                    min_samples_leaf=min_samples_leaf,
-                )
-                RF.fit(x_treino, y_treino)
-                opiniao = RF.predict(x_teste)
-                mse_validacao_RF = mean_squared_error(y_teste, opiniao)
-                
-                if (mse_validacao_RF < melhor_validacao_RF):
-                    melhor_validacao_RF = mse_validacao_RF
-                    best_paramsRF = {
-                        'criterion': criterion,
-                        'n_estimators': n_estimators,
-                        'min_samples_leaf': min_samples_leaf,
-                        'learning_rate_init': learning_rate_init
-                    }
+    for booster in param_grid['booster']:
+        XG = XGBRegressor(booster=booster)
+        XG.fit(x_treino, y_treino)
+        opiniao = XG.predict(x_teste)
+        mse_validacao_XG = mean_squared_error(y_teste, opiniao)
+        
+        if (mse_validacao_XG < melhor_validacao_XG):
+            melhor_validacao_XG = mse_validacao_XG
+            best_paramsXG = {
+                'booster': booster
+            }
 
 
-    print(melhor_validacao_RF)
-    print(mse_validacao_RF)
-    print(best_paramsRF)
+    print(melhor_validacao_XG)
+    print(mse_validacao_XG)
+    print(best_paramsXG)
 
-    RF = RFRegressor(
-                    criterion=best_paramsRF['criterion'],
-                    n_estimators=best_paramsRF['n_estimators'],
-                    solver='adam',
-                    min_samples_leaf=best_paramsRF['min_samples_leaf'],
-                    learning_rate_init=best_paramsRF['learning_rate_init']
-                )
+    XG = XGBRegressor(booster=best_paramsXG['booster'])
     
-    RF.fit(x_treino, y_treino)
-    opiniao_RF = RF.predict(x_teste)
+    XG.fit(x_treino, y_treino)
+    opiniao_XG = XG.predict(x_teste)
 
-    mae = mean_absolute_error(y_teste, opiniao_RF).round(2)
-    mse = mean_squared_error(y_teste, opiniao_RF).round(2)
+    mae = mean_absolute_error(y_teste, opiniao_XG).round(2)
+    mse = mean_squared_error(y_teste, opiniao_XG).round(2)
     rmse = np.sqrt(mse).round(2)
 
-    maeRF.append(mae)
-    mseRF.append(mse)
-    rmseRF.append(rmse)
+    maeXG.append(mae)
+    mseXG.append(mse)
+    rmseXG.append(rmse)
 
-    p_RFcriterion.append(best_paramsRF['criterion'])
-    p_RFn_estimators.append(best_paramsRF['n_estimators'])
-    p_RFmin_samples_leaf.append(best_paramsRF['min_samples_leaf'])
-    p_RFLearningRate.append(best_paramsRF['learning_rate_init'])
+    p_XGbooster.append(best_paramsXG['booster'])
+
 
 # RESULTADOS !!!!
-maxp_RFcriterion = max(set(p_RFcriterion), key=p_RFcriterion.count)
-maxp_RFn_estimators = max(set(p_RFn_estimators), key=p_RFn_estimators.count)
-maxp_RFmin_samples_leaf = max(set(p_RFmin_samples_leaf), key=p_RFmin_samples_leaf.count)
-maxp_RFLearningRate = max(set(p_RFLearningRate), key=p_RFLearningRate.count)
+maxp_XGbooster = max(set(p_XGbooster), key=p_XGbooster.count)
 
 
-media_maeRF = np.mean(maeRF)
-media_mseRF = np.mean(mseRF)
-media_rmseRF = np.mean(rmseRF)
+media_maeXG = np.mean(maeXG)
+media_mseXG = np.mean(mseXG)
+media_rmseXG = np.mean(rmseXG)
 
-p_RFcriterion.append(maxp_RFcriterion)
-p_RFn_estimators.append(p_RFn_estimators)
-p_RFmin_samples_leaf.append(p_RFmin_samples_leaf)
-p_RFLearningRate.append(p_RFLearningRate)
+p_XGbooster.append(maxp_XGbooster)
 
-maeRF.append(media_maeRF)
-mseRF.append(media_mseRF)
-rmseRF.append(media_rmseRF)
+maeXG.append(media_maeXG)
+mseXG.append(media_mseXG)
+rmseXG.append(media_rmseXG)
 
-dfRF = pd.DataFrame({
-    'criterion':p_RFcriterion,
-    'n_estimators':p_RFn_estimators,
-    'min_samples_leaf':p_RFmin_samples_leaf,
-    'MAE': maeRF,
-    'MSE': mseRF,
-    'RMSE': rmseRF
+dfXG = pd.DataFrame({
+    'booster':p_XGbooster,
+    'MAE': maeXG,
+    'MSE': mseXG,
+    'RMSE': rmseXG
 })
 
 print("O ÚLTIMO VALOR (20) É A MÉDIA !!!")
 
-display(dfRF)
-dfRF.to_csv('resultadosModelos/dfRF.csv', sep = ';', index = False, encoding='UTF-8')
+display(dfXG)
+dfXG.to_csv('resultadosModelos/dfXG.csv', sep = ';', index = False, encoding='UTF-8')
